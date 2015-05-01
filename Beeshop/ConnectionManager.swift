ba42d62fc0aa6cda03aa218b55946b611dbd25af
Mathieu Knecht <mathieuknecht@iMac-de-Mathieu-Knecht.local>
2015-05-01 14:09:10 +0200

    Add ConnectionManager

---
+++ Beeshop/ConnectionManager.swift
@@ -0,0 +1,22 @@
+//
+//  ConnectionManager.swift
+//  Beeshop
+//
+//  Created by Mathieu Knecht on 29.04.15.
+//  Copyright (c) 2015 Mathieu Knecht. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ConnectionManager {
+    
+    static let sharedInstance = ConnectionManager()
+    
+    init() {
+        
+    }
+    
+    
+    
+}
--- Podfile
+++ Podfile
@@ -1,8 +1,9 @@
 # Uncomment this line to define a global platform for your project
 # platform :ios, '8.0'
+use_frameworks!
 
 target 'Beeshop' do
-pod 'AFNetworking'
+pod 'Alamofire'
 end
 
 target 'BeeshopTests' do
